9a3114d66e7cb560f7e99aad6ae1613b800c7566
elekdaniel67 <elekdanipacek@gmail.com>
2026-04-30 23:58:26 +0200

    oojs menu
    Foci témájú objektum grafika kattintásra kilő egy labdát ami ha a "falnak" nekimegy visszapattan

--- react/mem/src/main.jsx
+++ react/mem/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './memoriajatek.jsx'
+import 'C:\Users\Elek Dániel\Documents\GitHub\WebProg-el-ad-s-beadand-\style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- react/ttt/src/main.jsx
+++ react/ttt/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './tictactoe.jsx'
+import 'C:\Users\Elek Dániel\Documents\GitHub\WebProg-el-ad-s-beadand-\style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
